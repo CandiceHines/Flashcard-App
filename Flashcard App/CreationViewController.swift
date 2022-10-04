ac841a1e71e1f8f7da767ffa3f79e18de95125a3
candicehines <candicehines17@gmail.com>
2022-10-03 21:57:22 -0700

    Updates flashcards

---
+++ Flashcard App/CreationViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CreationViewController.swift
+//  Flashcard App
+//
+//  Created by Candice Hines on 9/27/22.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    var flashcardController: ViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    @IBOutlet weak var answerChoice1Text: UITextField!
+    
+    @IBOutlet weak var answerChoice2Text: UITextField!
+    
+    @IBOutlet weak var answerChoice3Text: UITextField!
+    
+    @IBOutlet weak var answerChoice4Text: UITextField!
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText = questionTextField.text
+        let answerText = answerTextField.text
+        
+        flashcardController.updateFlashcard(question: questionText!, answer: answerText!)
+        
+        let answerOneText = answerChoice1Text.text
+        let answerTwoText = answerChoice2Text.text
+        let answerThreeText = answerChoice3Text.text
+        let answerFourText = answerChoice4Text.text
+        
+        flashcardController.updateAnswers(answerOne: answerOneText!, answerTwo: answerTwoText!, answerThree: answerThreeText!, answerFour: answerFourText!)
+        
+        flashcardController.initializeView()
+        dismiss(animated: true)
+    }
+
+}
--- Flashcard App/ViewController.swift
+++ Flashcard App/ViewController.swift
@@ -10,74 +10,155 @@ import SwiftUI
 
 class ViewController: UIViewController {
 
+
+
+    
+    @IBOutlet weak var answer1: UILabel!
+    @IBOutlet weak var answer2: UILabel!
+    @IBOutlet weak var answer3: UILabel!
+    @IBOutlet weak var answer4: UILabel!
+    
+    
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    var frontLabelPositionIsLeft = true
+    var backLabelPositionIsLeft = true
+    var didPressButton = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        question1.layer.cornerRadius = 15
-        question1.layer.shadowColor = UIColor.gray.cgColor
-        question1.layer.shadowOpacity = 1
-        question1.layer.shadowOffset = .zero
-        question1.layer.shadowRadius = 10
-        question1.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
-        question1.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        initializeView()
     }
-    @IBOutlet weak var Logo1: UITextField!
-    @IBOutlet weak var question1: UITextField!
-    @IBOutlet weak var logo: UITextField!
-    var question1PositionisLeft = true
-    @IBAction func ans1(_ sender: UIButton) {
+    
+    func initializeView() {
+        frontLabel.layer.masksToBounds = true
+        frontLabel.layer.cornerRadius = 15
+        frontLabel.layer.shadowColor = UIColor.gray.cgColor
+        frontLabel.layer.shadowOpacity = 1
+        frontLabel.layer.shadowOffset = .zero
+        frontLabel.layer.shadowRadius = 10
+        frontLabel.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+        frontLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
         
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 200, options: .curveEaseIn, animations: {
-            if self.question1PositionisLeft {
-                self.question1.center.x = self.view.bounds.width - 200
-            }
-            else {
-                self.question1.center.x = 200
-        }
-            self.question1PositionisLeft = !(self.question1PositionisLeft)
-        }, completion: nil)
-        sender.setTitle("SORRY, WRONG ANSWER!", for: .normal)
-        sender.setTitleColor(UIColor.red, for:.normal)
+        backLabel.layer.masksToBounds = true
+        backLabel.layer.cornerRadius = 15
+        backLabel.layer.shadowColor = UIColor.green.cgColor
+        backLabel.layer.shadowOpacity = 1
+        backLabel.layer.shadowOffset = .zero
+        backLabel.layer.shadowRadius = 10
+        backLabel.backgroundColor = UIColor.green.withAlphaComponent(0.1)
+        backLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        backLabel.isHidden = true
+        frontLabel.isHidden = false
     }
-    @IBAction func ans2(_ sender: UIButton) {
+    func updateFlashcard(question: String, answer: String){
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    func updateAnswers(answerOne: String, answerTwo: String, answerThree: String, answerFour: String){
+        answer1.text = answerOne
+        answer2.text = answerTwo
+        answer3.text = answerThree
+        answer4.text = answerFour
+    }
+    
+    @IBAction func didClickAnswerOne(_ sender: UIButton) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 200, options: .curveEaseIn, animations: {
-            if self.question1PositionisLeft {
-                self.question1.center.x = self.view.bounds.width - 200
+            if self.frontLabelPositionIsLeft {
+                self.frontLabel.center.x = self.view.bounds.width - 200
             }
             else {
-                self.question1.center.x = 200
+                self.frontLabel.center.x = 200
         }
-            self.question1PositionisLeft = !(self.question1PositionisLeft)
+            self.frontLabelPositionIsLeft = !(self.frontLabelPositionIsLeft)
         }, completion: nil)
-        question1.backgroundColor = UIColor.systemGreen.withAlphaComponent(0.3)
-        question1.text = ("ROME ✅")
-        sender.setTitle("CORRECT!!!", for: .normal)
         
+        if answer1.text == backLabel.text {
+            frontLabel.isHidden = true
+            backLabel.isHidden = false
+            answer1.text = "Correct!"
+        }
+        else if answer1.text != backLabel.text {
+            backLabel.isHidden = true
+            frontLabel.isHidden = false
+            answer1.text = "Sorry, wrong answer!"
+        }
     }
-    @IBAction func ans3(_ sender: UIButton) {
+    
+    @IBAction func didClickAnswerTwo(_ sender: UIButton) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 200, options: .curveEaseIn, animations: {
-            if self.question1PositionisLeft {
-                self.question1.center.x = self.view.bounds.width - 200
+            if self.frontLabelPositionIsLeft {
+                self.frontLabel.center.x = self.view.bounds.width - 200
             }
             else {
-                self.question1.center.x = 200
+                self.frontLabel.center.x = 200
         }
-            self.question1PositionisLeft = !(self.question1PositionisLeft)
+            self.frontLabelPositionIsLeft = !(self.frontLabelPositionIsLeft)
         }, completion: nil)
-        sender.setTitle("SORRY, WRONG ANSWER!", for: .normal)
-        sender.setTitleColor(UIColor.red, for:.normal)
+        
+        if answer2.text == backLabel.text {
+            frontLabel.isHidden = true
+            backLabel.isHidden = false
+            answer2.text = "Correct!"
+        }
+        else if answer2.text != backLabel.text {
+            backLabel.isHidden = true
+            frontLabel.isHidden = false
+            answer2.text = "Sorry, wrong answer!"
+        }
     }
-    @IBAction func ans4(_ sender: UIButton) {
+    
+    @IBAction func didClickAnswerThree(_ sender: UIButton) {
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 200, options: .curveEaseIn, animations: {
-            if self.question1PositionisLeft {
-                self.question1.center.x = self.view.bounds.width - 200
+            if self.frontLabelPositionIsLeft {
+                self.frontLabel.center.x = self.view.bounds.width - 200
             }
             else {
-                self.question1.center.x = 200
+                self.frontLabel.center.x = 200
         }
-            self.question1PositionisLeft = !(self.question1PositionisLeft)
+            self.frontLabelPositionIsLeft = !(self.frontLabelPositionIsLeft)
         }, completion: nil)
-        sender.setTitle("SORRY, WRONG ANSWER!", for: .normal)
-        sender.setTitleColor(UIColor.red, for:.normal)
+        
+        if answer3.text == backLabel.text {
+            frontLabel.isHidden = true
+            backLabel.isHidden = false
+            answer3.text = "Correct!"
+        }
+        else if answer3.text != backLabel.text {
+            backLabel.isHidden = true
+            frontLabel.isHidden = false
+            answer3.text = "Sorry, wrong answer!"
+        }
     }
-
+    
+    @IBAction func didClickAnswerFour(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 5, initialSpringVelocity: 200, options: .curveEaseIn, animations: {
+            if self.frontLabelPositionIsLeft {
+                self.frontLabel.center.x = self.view.bounds.width - 200
+            }
+            else {
+                self.frontLabel.center.x = 200
+        }
+            self.frontLabelPositionIsLeft = !(self.frontLabelPositionIsLeft)
+        }, completion: nil)
+        
+        if answer4.text == backLabel.text {
+            frontLabel.isHidden = true
+            backLabel.isHidden = false
+            answer4.text = "Correct!"
+        }
+        else if answer4.text != backLabel.text {
+            backLabel.isHidden = true
+            frontLabel.isHidden = false
+            answer4.text = "Sorry, wrong answer!"
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        creationController.flashcardController = self
+    }
 }
